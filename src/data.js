3fb155cdf738ef1a883471e71fd2396a1df7010a
YATIN SHARMA <sharmayatin0882@gmail.com>
2024-07-26 08:05:10 +0530

    adding all files

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data=
+[
+    {
+        id:1,
+        question:"What Is Your Name ?",
+        correct:"Yatin Sharma",
+        incorrect:["Himanshu Sharma","Diksha Sharma","Neha Sharma","Rahul Sharma"]
+    },
+    {
+        id:7,
+        question:"Are You Gay ?",
+        correct:"False",
+        incorrect:["True"]
+
+    },
+    {
+        id:2,
+        question:"What Is Your Age ?",
+        correct:"21",
+        incorrect:["22","28","30","23"]
+    },
+    {
+        id:3,
+        question:"What Is Your Gender ?",
+        correct:"Male",
+        incorrect:["Female","Gay","Trans","None Of The Above"]
+    },
+    {
+        id:4,
+        question:"What Is Your DOB ?",
+        correct:"21 April",
+        incorrect:["15 March","01 July","15 January","13 May"]
+    },
+    {
+        id:5,
+        question:"What Is Your Pet Name ?",
+        correct:"None Of The Above",
+        incorrect:["Jimmy","Tuffy","Nunnu","Google"]
+    },
+
+]
